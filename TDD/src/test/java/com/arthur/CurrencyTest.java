4923bf7f376377b2a8fb7ab19eef7ee17cc72a37
ysKim <znxkznxk1030@gmail.com>
2022-11-18 16:12:19 +0000

    프라이버시

--- TDD/src/test/java/com/arthur/CurrencyTest.java
+++ TDD/src/test/java/com/arthur/CurrencyTest.java
@@ -13,10 +13,8 @@ public class CurrencyTest {
     @DisplayName("곱셈 테스트")
     public void testMultiiplication() {
       Dollar five = new Dollar(5);
-      Dollar product = five.times(2);
-      assertEquals(10, product.amount);
-      product = five.times(3);
-      assertEquals(15, product.amount);
+      assertEquals(new Dollar(10), five.times(2));
+      assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
