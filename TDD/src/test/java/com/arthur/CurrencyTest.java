63d43e06333aa2e86b5f7b802309a4ba7bbc6591
ysKim <znxkznxk1030@gmail.com>
2022-11-18 16:20:41 +0000

    Franc과 Dollar를 비교 테스트 추가

--- TDD/src/test/java/com/arthur/CurrencyTest.java
+++ TDD/src/test/java/com/arthur/CurrencyTest.java
@@ -32,6 +32,8 @@ public class CurrencyTest {
       assertFalse(new Dollar(5).equals(new Dollar(6)));
       assertTrue(new Franc(5).equals(new Franc(5)));
       assertFalse(new Franc(5).equals(new Franc(6)));
+
+      assertFalse(new Franc(5).equals(new Dollar(5)));
     }
 }
 
